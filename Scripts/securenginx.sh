a6c5c63de374ac1fa32a44c7f07666dc580d0462
Jilty <49185299+Jilty@users.noreply.github.com>
2022-06-28 13:16:19 +0530

    Update securenginx.sh

--- Scripts/securenginx.sh
+++ Scripts/securenginx.sh
@@ -1,6 +1,8 @@
 #! bin/bash
 
-sudo apt-get install -y certbot python3-certbot-nginx
-read -p PROMPT jiltyissac2010@gmail.com
-sudo certbot --nginx -d istika.tk -d www.istika.tk
-read -p PROMPT 2
+certbot run -n --nginx --agree-tos -d istika.tk,www.istika.tk  -m  jiltyissac2010@gmail.com  --redirect
+
+# sudo apt-get install -y certbot python3-certbot-nginx
+# read -p PROMPT jiltyissac2010@gmail.com
+# sudo certbot --nginx -d istika.tk -d www.istika.tk
+# read -p PROMPT 2
