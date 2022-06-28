c9202e2c81d867e6384b3d7dcd41c50e999ec22e
Jilty <49185299+Jilty@users.noreply.github.com>
2022-06-28 11:03:47 +0530

    Update nginx.sh

--- Scripts/nginx.sh
+++ Scripts/nginx.sh
@@ -3,7 +3,7 @@
 sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
 
-cat /etc/nginx/sites-available/default | \
-sed -e 's/server_name _/server_name istika.tk www.istika.tk/' > /etc/nginx/sites-available/default
+# cat /etc/nginx/sites-available/default | \
+# sed -e 's/server_name _/server_name istika.tk www.istika.tk/' > /etc/nginx/sites-available/default
 
-sudo systemctl reload nginx
+# sudo systemctl reload nginx
