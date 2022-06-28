6b4ca3bb9e0c7128340a3f90fafca3df509d25f0
Jilty <49185299+Jilty@users.noreply.github.com>
2022-06-28 11:51:43 +0530

    Update nginx.sh

--- Scripts/nginx.sh
+++ Scripts/nginx.sh
@@ -5,4 +5,6 @@ sudo ufw allow 'Nginx HTTP'
 
 sed -e 's/server_name _/server_name istika.tk www.istika.tk/'  /etc/nginx/sites-available/default
 
+sudo ufw allow 'Nginx HTTP'
+
 # sudo systemctl reload nginx
